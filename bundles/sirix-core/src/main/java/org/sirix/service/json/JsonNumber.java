73c357f70f0e885cf1c24e13e1843c5b82583867
Mikael Carlsson <mikkar2@kth.se>
2020-02-13 13:06:02 +0100

    Complexity comments/JsonNumber.stringToNumber fix #41

--- bundles/sirix-core/src/main/java/org/sirix/service/json/JsonNumber.java
+++ bundles/sirix-core/src/main/java/org/sirix/service/json/JsonNumber.java
@@ -6,15 +6,20 @@ import java.math.BigInteger;
 public class JsonNumber {
     public static Number stringToNumber(String stringValue) {
         Number number;
-
+        //complexity is 1
         if (stringValue.contains(".")) {
+            //complexity is 2
             if (stringValue.contains("E") || stringValue.contains("e")) {
+                //complexity is 3
+                //complexity is 4
                 try {
                     number = Float.valueOf(stringValue);
                 } catch (final NumberFormatException eeee) {
+                    //complexity is 5
                     try {
                         number = Double.valueOf(stringValue);
                     } catch (final NumberFormatException eeeee) {
+                        //complexity is 6
                         throw new IllegalStateException(eeeee);
                     }
                 }
@@ -22,6 +27,7 @@ public class JsonNumber {
                 try {
                     number = new BigDecimal(stringValue);
                 } catch (final NumberFormatException eeeeee) {
+                    //complexity is 7
                     throw new IllegalStateException(eeeeee);
                 }
             }
@@ -29,12 +35,15 @@ public class JsonNumber {
             try {
                 number = Integer.valueOf(stringValue);
             } catch (final NumberFormatException e) {
+                //complexity is 8
                 try {
                     number = Long.valueOf(stringValue);
                 } catch (final NumberFormatException ee) {
+                    //complexity is 9
                     try {
                         number = new BigInteger(stringValue);
                     } catch (final NumberFormatException eee) {
+                        //complexity is 10
                         throw new IllegalStateException(eee);
                     }
                 }
